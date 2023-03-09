23786dfd74db755028d886781b316674f5782714
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-03-09 11:29:32 +0100

    lol

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+let fullName = "Hilda Johnson";
+
+console.log("The name is", fullName);
+
+let admin;
+
+let lastName;
+
+let firstName;
+
+let fullName2;
+
+let fullName3;
+
+
+
+
+firstName = "John";
+
+lastName = "Hansen";
+
+admin = firstName;
+
+fullName2 = firstName + " " + lastName;
+
+fullName3 = `${firstName} ${lastName}`;
+
+
+console.log(admin); 
+
+console.log(fullName2);
+
+console.log(fullName3);
+
+let fullName4 = `${firstName} ${lastName}`;
+console.log(fullName4);
